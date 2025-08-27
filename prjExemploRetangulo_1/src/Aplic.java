5d7ad00ac42493716b07237ddbedd0d7c9d06dd1
EdsssBarros <edsbarrosjr@gmail.com>
2025-08-26 22:03:32 -0300

    Projeto Retangulo Primeira Classe

---
+++ prjExemploRetangulo_1/src/Aplic.java
@@ -0,0 +1,20 @@
+/**
+ *
+ * @author edson.barros
+ */
+public class Aplic {
+    public static void main(String[] args) {
+        //Definição do ponteiro
+        Retangulo objRet;
+        
+        //Instanciação (alocação) de um objeto da classe Retangulo
+        objRet = new Retangulo();
+        
+        //Passagem de mensagem
+        objRet.setAltura(5.0);
+        objRet.setBase(8.0);
+        
+        System.out.println("Medida da área: " + objRet.calcArea());
+        System.out.println("Medida do perímetro " + objRet.calcPerimetro());
+    }
+}
