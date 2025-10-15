2ccc4a4001198719151bc205fdbd99be2ff34769
EdsssBarros <edsbarrosjr@gmail.com>
2025-10-14 22:06:54 -0300

    Exercício Lista Binária

---
+++ prjExemploAssociacaoBinaria/src/fatec/poo/model/Departamento.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fatec.poo.model;
+
+/**
+ *
+ * @author edson.barros
+ */
+public class Departamento {
+    
+    private String sigla;
+    private String nome;
+
+    public Departamento(String sigla, String nome) {
+        this.sigla = sigla;
+        this.nome = nome;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public String getNome() {
+        return nome;
+    }   
+}
+
